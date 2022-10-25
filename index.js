4a7e44cb8b32c2fafb35a65d1872e17237e6a13b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-10-24 21:51:20 -0400

    fix: use require.resolve on plugins

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 module.exports = {
   extends: [require.resolve('./a11y.js')],
   parser: '@typescript-eslint/parser',
-  plugins: ['react', 'jsx-expressions'],
+  plugins: [
+    require.resolve('eslint-plugin-react'),
+    require.resolve('eslint-plugin-jsx-expressions'),
+  ],
   rules: {
     'jsx-expressions/strict-logical-expressions': 'error',
     'react/jsx-key': [
